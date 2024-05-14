3a323ec6a2e88725191130d157982d8d2c2ac3a8
afifa <2023afifa@gmail.com>
2024-05-14 17:40:28 +0600

    Banner section done

--- src/Components/HomePage/Banner/Banner.jsx
+++ src/Components/HomePage/Banner/Banner.jsx
@@ -1,7 +1,14 @@
 const Banner = () => {
     return (
-        <div>
-            
+        <div className="flex md:justify-between md:items-center">
+            <div className="flex-1 pl-10">
+                <h1 className="text-5xl font-bold mb-5">Effortless Task Management: Organize, Collaborate, and Stay Productive</h1>
+                <p className="text-lg mb-5">Empower Your Team to Achieve More. Effortlessly Prioritize Tasks, Collaborate Seamlessly, and Stay on Track with Our Intuitive Task Management Platform.</p>
+                <button className="bg-purple-700 text-slate-200 py-3 px-5 rounded-3xl">Get Start</button>
+            </div>
+            <div className="w-3/5">
+                <img className="" src="https://i.ibb.co/bW0kXf2/Tiny-people-using-appointment-calendar-app-for-planning-events.jpg" alt="" />
+            </div>
         </div>
     );
 };
--- src/Components/HomePage/Navbar/Navbar.jsx
+++ src/Components/HomePage/Navbar/Navbar.jsx
@@ -1,32 +1,30 @@
 const Navbar = () => {
     return (
         <div>
-            <div className="mb-60">
-                <div className="navbar md:px-5 lg:px-10 bg-slate-50">
-                    <div className="md:navbar-start">
-                        <div className="flex-1">
-                            <h2 className="text-3xl font-bold uppercase">TaskPlus</h2>
-                        </div>
+            <div className="navbar md:px-5 lg:px-10 bg-transparent absolute">
+                <div className="md:navbar-start">
+                    <div className="flex-1">
+                        <h2 className="text-3xl font-bold uppercase">TaskPlus</h2>
                     </div>
-                    <div className="md:hidden flex flex-grow justify-end">
-                        <div className="dropdown navLink">
-                            <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-                            </div>
-                            <ul tabIndex={0} className="menu menu-sm dropdown-content right-3 z-[1] p-1 shadow bg-base-100 rounded-box w-28">
-                                <li>Home</li>
-                                <li>About</li>
-                            </ul>
+                </div>
+                <div className="md:hidden flex flex-grow justify-end">
+                    <div className="dropdown navLink">
+                        <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
                         </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content right-3 z-[1] p-1 shadow bg-base-100 rounded-box w-28">
+                            <li>Home</li>
+                            <li>About</li>
+                        </ul>
                     </div>
-                    <div className="navbar-end hidden md:flex">
-                        <div className="flex-none navLink">
-                            <ul className="menu menu-horizontal px-1 lg:text-lg font-medium space-x-7 items-center">
-                                <li>Home</li>
-                                <li>Login</li>
-                                <button className="bg-purple-700 text-slate-200 py-2 px-5 rounded-3xl">Join</button>
-                            </ul>
-                        </div>
+                </div>
+                <div className="navbar-end hidden md:flex">
+                    <div className="flex-none navLink">
+                        <ul className="menu menu-horizontal px-1 lg:text-lg font-medium space-x-7 items-center">
+                            <li>Home</li>
+                            <li>Login</li>
+                            <button className="bg-purple-700 text-slate-200 py-2 px-5 rounded-3xl">Join</button>
+                        </ul>
                     </div>
                 </div>
             </div>
