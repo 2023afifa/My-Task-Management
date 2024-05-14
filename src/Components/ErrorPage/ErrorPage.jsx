f40b6f3c15d0578f366403fac179e5b561db8667
afifa <2023afifa@gmail.com>
2024-05-14 13:00:35 +0600

    Error page done

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+    return (
+        <div>
+            <img className="max-h-screen mx-auto" src="../../../images/20945761.jpg" alt="" />
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from './Components/ErrorPage/ErrorPage.jsx';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <ErrorPage></ErrorPage>,
     element: <div>Hello world!</div>,
   },
 ]);
